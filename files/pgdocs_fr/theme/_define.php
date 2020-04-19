de198d95afb3e775aedfafda0ba1e4414945daa6
damien clochard <damien@dalibo.com>
2020-04-19 23:33:33 +0200

    [pgdocs_fr] v3

---
+++ files/pgdocs_fr/theme/_define.php
@@ -0,0 +1,18 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# Copyright (c) 2003-2008 Guillaume Lelarge
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */			"postgresql",
+	/* Description*/		"Thème spécifique de postgresql.org",
+	/* Author */			"Guillaume Lelarge",
+	/* Version */			'1.0'
+);
+?>
